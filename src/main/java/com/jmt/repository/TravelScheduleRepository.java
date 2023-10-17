3f12aa42ac00e919c858bb19fb7cba5acb38f199
eogns-2-dh <eogns2di@naver.com>
2023-10-17 17:49:13 +0900

    최초커밋

---
+++ src/main/java/com/jmt/repository/TravelScheduleRepository.java
@@ -0,0 +1,14 @@
+package com.jmt.repository;
+
+import com.jmt.entity.TravelScheduleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TravelScheduleRepository extends JpaRepository<TravelScheduleEntity, String> {
+
+    Optional<TravelScheduleEntity> findById(String travelId);
+
+
+
+}
