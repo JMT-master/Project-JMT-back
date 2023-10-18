de4d7ad15642a9434ab9c4e1ba7fe8f542b54c1f
elcrity <elcrity@naver.com>
2023-10-18 16:06:59 +0900

    Member 수정본으로 병합

--- src/main/java/com/jmt/controller/MemberController.java
+++ src/main/java/com/jmt/controller/MemberController.java
@@ -3,16 +3,13 @@ package com.jmt.controller;
 import com.jmt.dto.MemberDto;
 import com.jmt.entity.Member;
 import com.jmt.service.MemberService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.function.EntityResponse;
 
 @RestController
 @RequestMapping("/")
@@ -24,9 +21,8 @@ public class MemberController {
 
     @PostMapping("joinUser")
     public ResponseEntity<MemberDto> createMember(@RequestBody MemberDto dto){
-        System.out.println("dto = " + dto);
         service.validationUser(dto.getUsername());
-        Member entity = dto.toEntity(dto);
+        Member entity = MemberDto.toEntity(dto);
         entity.setUserid(null);
         if(entity == null){
             throw new RuntimeException("엔티티 이즈 널");
