f167ee95feb08ad2086d559c98c2ee798dd221ac
gwanghyeon <agh0625@gmail.com>
2023-10-27 10:39:49 +0900

    23년 10월 27일 오전 10시 40분 pdf 한글적용까지 완료

--- src/main/java/com/jmt/service/TravelPdfService.java
+++ src/main/java/com/jmt/service/TravelPdfService.java
@@ -1,15 +1,24 @@
 package com.jmt.service;
 
+import com.itextpdf.io.font.FontProgram;
+import com.itextpdf.io.font.FontProgramFactory;
+import com.itextpdf.io.font.PdfEncodings;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.font.FontProvider;
 import com.jmt.entity.Travel;
 import com.jmt.entity.TravelPlan;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.ByteArrayOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
@@ -23,14 +32,26 @@ public class TravelPdfService {
         PdfDocument pdf = new PdfDocument(pdfWriter);
         Document document = new Document(pdf);
 
+        // Google 폰트 로딩
+        Path fontFilePath = Path.of("D:\\final-project\\final\\src\\main\\resources\\static\\Nanum_Gothic\\NanumGothic-Regular.ttf"); // 다운로드한 폰트 파일의 경로를 지정하세요
+        byte[] fontBytes = Files.readAllBytes(fontFilePath);
+
+        // 문서에 폰트 설정
+        FontProgram fontProgram = FontProgramFactory.createFont(fontBytes);
+        PdfFont customFont = PdfFontFactory.createFont(fontProgram, PdfEncodings.IDENTITY_H, PdfFontFactory.EmbeddingStrategy.PREFER_EMBEDDED);
+
+
         System.out.println("서비스에서 확인하는 travelList = " + travelList);
 //        System.out.println("서비스에서 확인하는 travelList.getTravels = " + travelList.getTravels());
 
         //여행 일정 데이터를 PDF에 추가하는 방법 다시
         for (Travel travel : travelList){
-            document.add(new Paragraph("날짜 : " + travel.getStartTime()));
-            document.add(new Paragraph("주소 : " + travel.getPlace()));
-            document.add(new Paragraph("장소 이름 : " + travel.getPlaceName()));
+            document.add(new Paragraph("날짜 : " + travel.getStartTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd : HH:mm")))
+                    .setFont(customFont));
+            document.add(new Paragraph("주소 : " + travel.getPlace())
+                    .setFont(customFont));
+            document.add(new Paragraph("장소 이름 : " + travel.getPlaceName())
+                    .setFont(customFont));
             document.add(new Paragraph("------------------"));
         }
         document.close();
