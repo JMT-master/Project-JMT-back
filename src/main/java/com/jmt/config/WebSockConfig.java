86e681d65e51fe69ec7736b9973590160f4828d6
gwanghyeon <agh0625@gmail.com>
2023-10-31 17:16:19 +0900

    안광현 Deb 머지완료

--- src/main/java/com/jmt/config/WebSockConfig.java
+++ src/main/java/com/jmt/config/WebSockConfig.java
@@ -4,19 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
-
-//@RequiredArgsConstructor
-//@Configuration
-//@EnableWebSocket
-//public class WebSockConfig implements WebSocketConfigurer {
-//
-//    private final WebSockChatHandler webSockChatHandler;
-//
-//    @Override
-//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-//        registry.addHandler(webSockChatHandler, "/ws/chat").setAllowedOrigins("*");
-//    }
-//}
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSocketMessageBroker
