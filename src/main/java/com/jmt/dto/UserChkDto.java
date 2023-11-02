68b579ad28d3340755a5f7db86fd7e53f7563d54
nc199 <nc199@hanmail.net>
2023-11-02 17:41:26 +0900

    231102 1730

---
+++ src/main/java/com/jmt/dto/UserChkDto.java
@@ -0,0 +1,11 @@
+package com.jmt.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserChkDto {
+    private Boolean isSameUser;
+    private Boolean isAdmin;
+}
