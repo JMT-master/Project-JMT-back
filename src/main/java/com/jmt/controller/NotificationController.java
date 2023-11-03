43ecebfc24e02500468fdfa42bed542278b3c3c9
elcrity <elcrity@naver.com>
2023-11-03 16:02:41 +0900

    리뷰 업데이트시 업데이트 -> 전체 반환으로 갱신으로 변경중

--- src/main/java/com/jmt/controller/NotificationController.java
+++ src/main/java/com/jmt/controller/NotificationController.java
@@ -2,20 +2,16 @@ package com.jmt.controller;
 
 import com.jmt.common.TokenProvidor;
 import com.jmt.dto.NotificationDto;
-import com.jmt.entity.Notification;
 import com.jmt.entity.Member;
+import com.jmt.entity.Notification;
 import com.jmt.service.EmitterService;
-import com.jmt.service.NotificationService;
 import com.jmt.service.MemberService;
+import com.jmt.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -37,46 +33,53 @@ public class NotificationController {
     private final EmitterService emitterService;
 
 
-
-    @GetMapping(value = "/sub",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter subscribe(@AuthenticationPrincipal String email){
-        SseEmitter checkEmitter= emitterService.subscribe(email);
+    @GetMapping(value = "/sub", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter subscribe(@AuthenticationPrincipal String email) {
+        SseEmitter checkEmitter = emitterService.subscribe(email);
+        if(emitterService.chkEmitter(email)){
+            throw new RuntimeException("이미 존재하는 이미터");
+        }
         log.debug("emitter sub chekc : " + checkEmitter);
         return checkEmitter;
     }
+
     @PostMapping("/send")
     public ResponseEntity<NotificationDto> sendData(
+            @AuthenticationPrincipal String email,
             @RequestBody NotificationDto dto) {
+
         Notification notification = NotificationDto.toEntity(dto);
-        log.debug("senddto" + dto);
-        Member member = memberService.getMember(dto.getUserid());
-        notification.setMember(member);
-        notificationService.addNotification(notification);
+        if (!dto.getUserid().equals(email)) {
+            log.debug("senddto" + dto);
+            Member member = memberService.getMember(dto.getUserid());
+            notification.setMember(member);
 
-        emitterService.send(member.getEmail(),dto);
-
+            notificationService.addNotification(notification);
+            emitterService.send(member.getEmail(), dto);
+        }
         NotificationDto notificationDto = NotificationDto.toDto(notification);
         return ResponseEntity.ok().body(notificationDto);
     }
 
     @Transactional
     @PutMapping
-    public ResponseEntity<NotificationDto> regNotification(@RequestBody NotificationDto dto){
+    public ResponseEntity<NotificationDto> regNotification(@RequestBody NotificationDto dto) {
         Notification entity = NotificationDto.toEntity(dto);
 //        entity.setMember(memberService.getMember(tokenProvidor.getUserId(token)));
         log.info("entity : " + entity);
         NotificationDto alarmDto = NotificationDto.toDto(entity);
-        if(entity == null){
+        if (entity == null) {
             throw new RuntimeException("엔티티 이즈 널");
         }
         try {
             notificationService.addNotification(entity);
             return ResponseEntity.ok().body(alarmDto);
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error(e.getMessage());
             return ResponseEntity.badRequest().body(alarmDto);
         }
     }
+
     @Transactional
     @PutMapping("/isRead")
     public ResponseEntity<List<NotificationDto>> isReadNotify(@RequestBody NotificationDto dto, @AuthenticationPrincipal String userid) {
@@ -96,64 +99,67 @@ public class NotificationController {
             return ResponseEntity.badRequest().body(entity);
         }
     }
+
     @Transactional
     @PostMapping
-    public ResponseEntity<List<NotificationDto>> readAllNotification(@AuthenticationPrincipal String userid){
+    public ResponseEntity<List<NotificationDto>> readAllNotification(@AuthenticationPrincipal String userid) {
         List<Notification> entities = notificationService.showNotification(userid);
         log.debug("===============entities : " + entities);
         List<NotificationDto> entity = new ArrayList<>();
 
-        for(Notification notification : entities){
+        for (Notification notification : entities) {
             entity.add(NotificationDto.toDto(notification));
         }
 
         System.out.println("entity = " + entity);
 
         System.out.println("ResponseEntity.ok().body(entity); = " + ResponseEntity.ok().body(entity));
-        if(entities == null){
+        if (entities == null) {
             throw new RuntimeException("엔티티 이즈 널");
         }
         try {
             return ResponseEntity.ok().body(entity);
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error(e.getMessage());
             return ResponseEntity.badRequest().body(entity);
         }
     }
+
     @Transactional
     @DeleteMapping
-    public ResponseEntity<List<NotificationDto>> removeNotification(@RequestBody NotificationDto dto, @AuthenticationPrincipal String userid){
+    public ResponseEntity<List<NotificationDto>> removeNotification(@RequestBody NotificationDto dto, @AuthenticationPrincipal String userid) {
         notificationService.deleteNotification(dto.getUserid());
         List<Notification> entities = notificationService.showNotification(userid);
 
         List<NotificationDto> entity = new ArrayList<>();
-        for(Notification notification : entities){
+        for (Notification notification : entities) {
             entity.add(NotificationDto.toDto(notification));
         }
         try {
             return ResponseEntity.ok().body(entity);
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error(e.getMessage());
             return ResponseEntity.badRequest().body(entity);
         }
     }
+
     @Transactional
     @DeleteMapping("/all")
-    public ResponseEntity<List<NotificationDto>> removeAllNotification(@AuthenticationPrincipal String userid){
+    public ResponseEntity<List<NotificationDto>> removeAllNotification(@AuthenticationPrincipal String userid) {
         notificationService.deleteAllNotification(userid);
 
         List<Notification> entities = notificationService.showNotification(userid);
         List<NotificationDto> entity = new ArrayList<>();
 
-        for(Notification notification : entities){
+        for (Notification notification : entities) {
             entity.add(NotificationDto.toDto(notification));
         }
-        if(entities == null){
+        if (entities == null) {
             throw new RuntimeException("엔티티가 널임");
         }
         try {
             return ResponseEntity.ok().body(entity);
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error(e.getMessage());
             return ResponseEntity.badRequest().body(entity);
         }
